9b7b53807882053a6f82b0d4dbcc11698c0be554
SonsoriIssah <issonsori@gmail.com>
2026-08-29 17:04:29 +0000

    feat: add async database setup with SQLAlchemy engine and session dependency

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession,async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+DATABASE_URL = os.environ['DATABASE_URL']
+engine = create_async_engine(DATABASE_URL,echo=True)
+AsyncSessionLocal = async_sessionmaker(engine,expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
